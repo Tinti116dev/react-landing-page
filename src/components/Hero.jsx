223fffc58eb3c65a9d4bb78557147390caa05a1a
Tinti116dev <tinamutuswa@gmail.com>
2025-10-03 11:19:40 +0200

    Resolved merge conflicts

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 import { Link } from "react-router-dom";
-=======
->>>>>>> c1f267c04fa0fee5daf29c6c3cf59591a8484c4a
 import dashboardImg from "../assets/images/dashboard.jpg"; // adjust path if needed
 
 function Hero() {
   return (
-<<<<<<< HEAD
-    <section id="hero" className="pt-28 pb-16 text-white">
-      {/* removed bg-black */}
-=======
-    <section id="hero" className="pt-28 pb-16 bg-black text-white">
->>>>>>> c1f267c04fa0fee5daf29c6c3cf59591a8484c4a
+<section id="hero" className="pt-28 pb-16 bg-black text-white">
       <div className="max-w-7xl mx-auto px-6 grid md:grid-cols-2 gap-12 items-center">
         {/* Left Content */}
         <div>
@@ -29,7 +21,6 @@ function Hero() {
 
           {/* Buttons */}
           <div className="flex space-x-4">
-<<<<<<< HEAD
             <Link
               to="/demo"
               className="px-5 py-3 bg-blue-600 rounded-lg font-semibold text-white hover:bg-blue-700 transition"
@@ -42,20 +33,6 @@ function Hero() {
             >
               Explore Features
             </Link>
-=======
-            <a
-              href="#start"
-              className="px-5 py-3 bg-blue-600 rounded-lg font-semibold text-white hover:bg-blue-700 transition"
-            >
-              Start Free Trial
-            </a>
-            <a
-              href="#demo"
-              className="px-5 py-3 bg-blue-600 rounded-lg font-semibold text-white hover:bg-blue-700 transition"
-            >
-              Watch Demo
-            </a>
->>>>>>> c1f267c04fa0fee5daf29c6c3cf59591a8484c4a
           </div>
         </div>
 
